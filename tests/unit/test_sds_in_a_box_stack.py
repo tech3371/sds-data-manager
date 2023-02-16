102972a88949a3e4a8d3d1c2eeaab64228e365ae
Bryan Harter <harter864@gmail.com>
2023-02-16 16:42:03 -0700

    Adding in item for unit testing

--- tests/unit/test_sds_in_a_box_stack.py
+++ tests/unit/test_sds_in_a_box_stack.py
@@ -4,13 +4,12 @@
 
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-
 from sds_in_a_box.sds_in_a_box_stack import SdsInABoxStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in sds_in_a_box/sds_in_a_box_stack.py
 def test_sqs_queue_created():
-    app = core.App()
+    app = core.App(context={"SDSID": "unit-testing"})
     stack = SdsInABoxStack(app, "sds-in-a-box")
     template = assertions.Template.from_stack(stack)
 
