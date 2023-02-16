03a4aa3c4cca4d8758045dbc2bbd95b8cc7f54b4
Bryan Harter <harter864@gmail.com>
2023-02-16 16:19:55 -0700

    Adding context in a different way

--- tests/unit/test_sds_in_a_box_stack.py
+++ tests/unit/test_sds_in_a_box_stack.py
@@ -9,7 +9,7 @@ from sds_in_a_box.sds_in_a_box_stack import SdsInABoxStack
 # example tests. To run these tests, uncomment this file along with the example
 # resource in sds_in_a_box/sds_in_a_box_stack.py
 def test_sqs_queue_created():
-    app = core.App()
+    app = core.App(context={"SDS_ID": "unit-testing"})
     app_props = core.AppProps(context={"SDS_ID": "unit-testing"})
     stack = SdsInABoxStack(app, "sds-in-a-box")
     template = assertions.Template.from_stack(stack)
