ac8f464af92e066844be518d2f6494f7fb089389
Bryan Harter <harter864@gmail.com>
2023-02-16 16:26:09 -0700

    Typo

--- tests/unit/test_sds_in_a_box_stack.py
+++ tests/unit/test_sds_in_a_box_stack.py
@@ -9,8 +9,7 @@ from sds_in_a_box.sds_in_a_box_stack import SdsInABoxStack
 # example tests. To run these tests, uncomment this file along with the example
 # resource in sds_in_a_box/sds_in_a_box_stack.py
 def test_sqs_queue_created():
-    app = core.App(context={"SDS_ID": "unit-testing"})
-    app_props = core.AppProps(context={"SDS_ID": "unit-testing"})
+    app = core.App(context={"SDSID": "unit-testing"})
     stack = SdsInABoxStack(app, "sds-in-a-box")
     template = assertions.Template.from_stack(stack)
 
