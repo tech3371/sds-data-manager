63c3c2203791547b82a7df218b537e6b353659d0
Bryan Harter <harter864@gmail.com>
2023-02-17 14:37:50 -0700

    Adding the upload API to the stack and adding the api code

--- sds_in_a_box/sds_in_a_box_stack.py
+++ sds_in_a_box/sds_in_a_box_stack.py
@@ -115,6 +115,33 @@ class SdsInABoxStack(Stack):
                                                       )
                                         )
         indexer_lambda.apply_removal_policy(cdk.RemovalPolicy.DESTROY)
+        
+        # Adding a lambda for uploading files to the SDS
+        upload_api_lambda = lambda_alpha_.PythonFunction(self,
+                                      id="APILambda",
+                                      entry=os.path.join(os.path.dirname(os.path.realpath(__file__)), "SDSCode/"),
+                                      index="upload_api.py",
+                                      handler="lambda_handler",
+                                      runtime=lambda_.Runtime.PYTHON_3_9,
+                                      timeout=cdk.Duration.minutes(15),
+                                      memory_size=1000,
+                                      environment={"OS_ADMIN_USERNAME": "master-user", 
+                                                   "OS_ADMIN_PASSWORD_LOCATION": os_secret.secret_name,
+                                                   "S3_BUCKET": data_bucket.s3_url_for_object()},
+        )
+        # Adding S3 Permissions 
+        upload_api_lambda.add_to_role_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["s3:*"],
+                resources=[
+                    f"{data_bucket.bucket_arn}/*"
+                ],
+            )
+        )
+        upload_api_lambda.apply_removal_policy(cdk.RemovalPolicy.DESTROY)
+        upload_api_url = upload_api_lambda.add_function_url(auth_type=lambda_.FunctionUrlAuthType.NONE,
+                                              cors=lambda_.FunctionUrlCorsOptions(allowed_origins=["*"]))
 
         # The purpose of this lambda function is to trigger off of a lambda URL.
         query_api_lambda = lambda_alpha_.PythonFunction(self,
@@ -164,3 +191,7 @@ class SdsInABoxStack(Stack):
                                 allowed_origins=["*"],
                                 allowed_methods=[lambda_.HttpMethod.GET])
         )
+
+########### OUTPUTS
+        # This is a list of the major outputs of the stack
+        cdk.CfnOutput(self, "UPLOAD_API_URL", value=upload_api_url.url)
