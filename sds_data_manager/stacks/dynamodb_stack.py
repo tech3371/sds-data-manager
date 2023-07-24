c1e0a998784c9ddb29d45fc465cb7c1cc00a0377
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2023-07-24 14:42:25 -0600

    refactored code to be simpler based on feedback. Refactored Tests to include provisioned DynamoDB tests.

--- sds_data_manager/stacks/dynamodb_stack.py
+++ sds_data_manager/stacks/dynamodb_stack.py
@@ -1,5 +1,5 @@
-# Create stack for dynamoDb
-from aws_cdk import RemovalPolicy, Stack
+# Create stack for DynamoDB
+from aws_cdk import Environment, RemovalPolicy, Stack
 from aws_cdk import aws_dynamodb as dynamodb
 from constructs import Construct
 
@@ -13,10 +13,10 @@ class DynamoDB(Stack):
         table_name: str,
         partition_key: str,
         sort_key: str,
-        env,
-        on_demand=True,
-        read_capacity=1,
-        write_capacity=1,
+        env: Environment,
+        on_demand: bool = True,
+        read_capacity: int = None,
+        write_capacity: int = None,
         **kwargs,
     ):
         super().__init__(scope, construct_id, **kwargs)
@@ -53,12 +53,12 @@ class DynamoDB(Stack):
         env : Environment
             Account and region
         on_demand : bool
-            If true, creates on demand DynamoDb table. If false, creates provisioned DynamoDb table.
+            If true, creates on demand DynamoDB table. If false, creates provisioned DynamoDB table.
         read_capacity : int
-            Read capacity for provisioned DynamoDb table.
+            Read capacity for provisioned DynamoDB table.
             Default value is 1.
         write_capacity : int
-            Write capacity for provisioned DynamoDb table.
+            Write capacity for provisioned DynamoDB table.
             Default value is 1.
         """
         self.sds_id = sds_id
@@ -66,61 +66,25 @@ class DynamoDB(Stack):
         self.partition_key = partition_key
         self.sort_key = sort_key
         self.on_demand = on_demand
-        if self.on_demand:
-            self._create_on_demand_dynamodb()
-        else:
-            self._create_provisioned_dynamodb(read_capacity, write_capacity)
 
-    def _create_on_demand_dynamodb(self):
-        """Creates On Demand DynamoDb table. On Demand DynamoDb table is created with PAY_PER_REQUEST billing mode.
-
-        When you turn on point-in-time recovery (PITR), DynamoDB backs up your table data automatically so that you
-        can restore to any given second in the preceding 35 days.
-        Returns
-        -------
-        Construct
-            DynamoDb table construct
-        """
-        return dynamodb.Table(
-            self,
-            f"OnDemandDynamoDb-{self.sds_id}",
-            table_name=self.table_name,
-            partition_key=dynamodb.Attribute(
-                name=self.partition_key, type=dynamodb.AttributeType.STRING
-            ),
-            sort_key=dynamodb.Attribute(
-                name=self.sort_key, type=dynamodb.AttributeType.STRING
-            ),
-            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=RemovalPolicy.DESTROY,
-            point_in_time_recovery=True,
-        )
-
-    def _create_provisioned_dynamodb(self, read_capacity: int, write_capacity: int):
-        """Creates Provisioned DynamoDb table. Provisioned DynamoDb table is created with PROVISIONED billing mode.
-
-        When you turn on point-in-time recovery (PITR), DynamoDB backs up your table data automatically so that you
-        can restore to any given second in the preceding 35 days.
-
-        Parameters
-        ----------
-        read_capacity : int
-            Read capacity for provisioned DynamoDb table.
-        write_capacity : int
-            Write capacity for provisioned DynamoDb table.
-
-        Returns
-        -------
-        Construct
-            DynamoDb table construct
-        """
-        if read_capacity == 1 and write_capacity == 1:
-            raise Warning(
+        if not on_demand and read_capacity is None and write_capacity is None:
+            raise ValueError(
                 "Read capacity and write capacity are using default values. Please use a different value"
             )
-        return dynamodb.Table(
+
+        if on_demand:
+            # On Demand DynamoDB table is created with PAY_PER_REQUEST billing mode.
+            billing_mode = dynamodb.BillingMode.PAY_PER_REQUEST
+        else:
+            # Provisioned DynamoDB table is created with PROVISIONED billing mode.
+            billing_mode = dynamodb.BillingMode.PROVISIONED
+
+        # When you turn on point-in-time recovery (PITR), DynamoDB backs up your table
+        # data automatically so that you can restore to any given second in the preceding
+        # 35 days
+        dynamodb.Table(
             self,
-            f"ProvisionedDynamoDb-{self.sds_id}",
+            f"DynamoDB-{self.sds_id}",
             table_name=self.table_name,
             partition_key=dynamodb.Attribute(
                 name=self.partition_key, type=dynamodb.AttributeType.STRING
@@ -128,12 +92,9 @@ class DynamoDB(Stack):
             sort_key=dynamodb.Attribute(
                 name=self.sort_key, type=dynamodb.AttributeType.STRING
             ),
-            billing_mode=dynamodb.BillingMode.PROVISIONED,
+            billing_mode=billing_mode,
             write_capacity=write_capacity,
             read_capacity=read_capacity,
             removal_policy=RemovalPolicy.DESTROY,
             point_in_time_recovery=True,
         )
-
-    def get_table_arn(self, table_name):
-        return dynamodb.Table.from_table_name(self, table_name, table_name)
