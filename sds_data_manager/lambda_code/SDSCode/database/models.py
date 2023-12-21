2508932545b2cd8162b730f0284555d25cd786d5
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2023-12-21 16:04:38 -0700

    Metadata Schema (#209)
    * added initial metadata schema
    
    * fixed inheritance for common metadata table
    
    * added string cap for level and instrument
    
    * added more to table TODO
    
    * changed table names to file catalog

--- sds_data_manager/lambda_code/SDSCode/database/models.py
+++ sds_data_manager/lambda_code/SDSCode/database/models.py
@@ -1,10 +1,5 @@
 """Main file to store schema definition"""
-from sqlalchemy import (
-    Boolean,
-    Column,
-    DateTime,
-    Integer,
-)
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -27,3 +22,84 @@ class UniversalSpinTable(Base):
     # TODO: create table for repointing and then make
     # a foreign key to universal_spin_table
     repointing_number = Column(Integer, nullable=False)
+
+
+class FileCatalogTable:
+    """Common File Catalog table"""
+
+    # TODO: determine cap for strings
+    id = Column(Integer, primary_key=True)
+    file_path = Column(String, nullable=False)
+    instrument = Column(String(6), nullable=False)
+    data_level = Column(String(3), nullable=False)
+    descriptor = Column(String, nullable=False)
+    start_date = Column(DateTime(timezone=True), nullable=False)
+    end_date = Column(DateTime(timezone=True), nullable=False)
+    ingestion_date = Column(DateTime(timezone=True), nullable=False)
+    version = Column(Integer, nullable=False)
+    extension = Column(String, nullable=False)
+
+
+# TODO: Follow-up PR should add in columns for each instrument
+# for instrument dependency IDs, SPICE ID, parent id,
+# and pointing id
+
+
+class LoTable(FileCatalogTable, Base):
+    """IMAP-Lo File Catalog Table"""
+
+    __tablename__ = "lo"
+
+
+class HiTable(FileCatalogTable, Base):
+    """IMAP-Hi File Catalog Table"""
+
+    __tablename__ = "hi"
+
+
+class UltraTable(FileCatalogTable, Base):
+    """IMAP-Ultra File Catalog Table"""
+
+    __tablename__ = "ultra"
+
+
+class HITTable(FileCatalogTable, Base):
+    """HIT File Catalog Table"""
+
+    __tablename__ = "hit"
+
+
+class IDEXTable(FileCatalogTable, Base):
+    """IDEX File Catalog Table"""
+
+    __tablename__ = "idex"
+
+
+class SWAPITable(FileCatalogTable, Base):
+    """SWAPI File Catalog Table"""
+
+    __tablename__ = "swapi"
+
+
+class SWETable(FileCatalogTable, Base):
+    """SWE File Catalog Table"""
+
+    __tablename__ = "swe"
+
+
+class CoDICETable(FileCatalogTable, Base):
+    """CoDICE File Catalog Table"""
+
+    __tablename__ = "codice"
+
+
+class MAGTable(FileCatalogTable, Base):
+    """MAG File Catalog Table"""
+
+    __tablename__ = "mag"
+
+
+class GLOWSTable(FileCatalogTable, Base):
+    """GLOWS File Catalog Table"""
+
+    __tablename__ = "glows"
