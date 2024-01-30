061cba3d5a77f465724e16af91feb580c522812c
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-01-30 11:08:37 -0700

    updated imap_api

--- tests/docker_images/imap_api.py
+++ tests/docker_images/imap_api.py
@@ -3,9 +3,8 @@
 import argparse
 import json
 import logging
-from pathlib import Path
 
-import requests
+import imap_data_access
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,9 +20,9 @@ def _parse_args():
 
     description = (
         "This command line program downloads"
-        "a file from the s3_uri, modifies it, and uploads"
-        "the modified file to the same s3 uri. "
-        "Example usage: python cli.py <s3_uri>. "
+        "a file from the file_path, modifies it, and uploads"
+        "the modified file to the same s3 path. "
+        "Example usage: python cli.py <file_path>. "
     )
 
     api_endpoint_help = (
@@ -39,7 +38,7 @@ def _parse_args():
         "--level", type=str, required=True, help="Data processing level."
     )
     parser.add_argument(
-        "--s3_uri",
+        "--file_path",
         type=str,
         required=True,
         help="Full path to the file in the S3 bucket.",
@@ -56,87 +55,26 @@ def _parse_args():
     return args
 
 
-def download(s3_uri, api_endpoint="https://api.dev.imap-mission.com"):
-    """
-    Download a file from a given S3 URI via the specified API endpoint.
+def main():
+    """Main function for the IMAP API utilities."""
+    args = _parse_args()
 
-    Parameters
-    ----------
-    s3_uri : str
-        The S3 URI of the file to be downloaded.
-    api_endpoint : str, optional
-        The API endpoint to use for downloading the file.
-
-    Returns
-    -------
-    file_name_and_path : str
-        The file path where the downloaded file is saved.
-    """
-    logging.info(f"Starting download from S3 URI: {s3_uri}")
-
-    url_with_parameters = f"{api_endpoint}/download?{s3_uri}"
-    response = requests.get(url_with_parameters)
-
-    # Set the base directory
-    base_directory = Path("/mnt/data")
-
-    if not base_directory.exists():
-        base_directory = Path.cwd()
-
-    # Join the base directory with the file name
-    file_name_and_path = base_directory / Path(s3_uri.replace("s3://", ""))
-
-    # Make parent directories if they don't exist
-    file_name_and_path.parent.mkdir(parents=True, exist_ok=True)
-
-    with open(file_name_and_path, "wb") as file:
-        file.write(response.content)
-
-    logging.info(f"File downloaded and saved to: {file_name_and_path}")
-
-    return str(file_name_and_path)
-
-
-def upload(local_file_location, api_endpoint="https://api.dev.imap-mission.com"):
-    """
-    Upload a local file to a remote server using the specified API endpoint.
-
-    Parameters
-    ----------
-    local_file_location : str
-        The file path of the file to be uploaded.
-    api_endpoint : str, optional
-        The API endpoint to use for uploading the file.
-    """
-    logging.info(f"Starting upload for file: {local_file_location}")
-
-    local_file_path = Path(local_file_location)
-    remote_file_name = local_file_path.name
+    # Download file
+    output_path = imap_data_access.download(args.file_path)
+    # change filename
+    remote_file_name = output_path.name
 
     # Modify descriptor in file name
     modified_file_name = remote_file_name.replace(
         remote_file_name[12:15], f"{remote_file_name[12:15]}-test"
     )
-    # Upload the file
-    url_with_parameters = f"{api_endpoint}/upload?filename={modified_file_name}"
-    get_response = requests.get(url_with_parameters)
-    upload_url = get_response.json()
-    requests.put(upload_url)
-
-    logging.info(f"File uploaded: {modified_file_name}")
-
-
-def main():
-    """Main function for the IMAP API utilities."""
-    args = _parse_args()
-
-    endpoint = (
-        args.api_endpoint
-        if args.api_endpoint is not None
-        else "https://api.dev.imap-mission.com"
-    )
-    file_name_and_path = download(args.s3_uri, endpoint)
-    upload(file_name_and_path, endpoint)
+    new_path = output_path.parent / modified_file_name
+    # Create an empty file
+    new_path.touch(exist_ok=True)
+    logging.info(f"renamed file {modified_file_name}")
+    logging.info(f"new path - {new_path}")
+    # upload renamed file
+    imap_data_access.upload(new_path)
 
     logging.info("Process completed successfully")
 
