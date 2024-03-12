b282a033cdca79d503e5b009366ff6e1c0cc87bd
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-03-12 12:12:32 -0600

    commented out ECR push permissions

--- sds_data_manager/stacks/ecr_stack.py
+++ sds_data_manager/stacks/ecr_stack.py
@@ -2,7 +2,6 @@
 
 from aws_cdk import RemovalPolicy, Stack
 from aws_cdk import aws_ecr as ecr
-from aws_cdk import aws_iam as iam
 from constructs import Construct
 
 
@@ -40,19 +39,26 @@ class EcrStack(Stack):
             image_scan_on_push=True,
         )
 
-        # Grant access to developers to push ECR Images to be used by the batch job
-        ecr_authenticators = iam.Group(self, "EcrAuthenticators")
+        # TODO: remove these lines or find replacement. This is causing an error.
+        # # Grant access to developers to push ECR Images to be used by the batch job
+        # ecr_authenticators = iam.Group(self, "EcrAuthenticators")
 
-        # Allows members of this group to get the auth token for `docker login`
-        ecr.AuthorizationToken.grant_read(ecr_authenticators)
+        # # Allows members of this group to get the auth token for `docker login`
+        # ecr.AuthorizationToken.grant_read(ecr_authenticators)
 
-        # Grant permissions to the group to pull and push images
-        self.container_repo.grant_pull_push(ecr_authenticators)
+        # # Grant permissions to the group to pull and push images
+        # self.container_repo.grant_pull_push(ecr_authenticators)
 
         # Add each of the SDC devs to the newly created group
         # TODO: should we remove this?
-        for username in self.node.try_get_context("usernames"):
-            user = iam.User.from_user_name(self, username, user_name=username)
-            ecr_authenticators.add_user(user)
+        # Error from this line:
+        # The stack named loEcr failed creation, it may need to
+        # be manually deleted from the AWS console: ROLLBACK_COMPLETE:
+        # The maximum number of groups per user is exceeded for user: sandoval.
+        # (Service: AmazonIdentityManagement; Status Code: 409; Error Code:
+        # LimitExceeded; Request ID: 71ba3c0c-2be1-45e8-87cd-f1645fa49a94; Proxy: null)
+        # for username in self.node.try_get_context("usernames"):
+        #     user = iam.User.from_user_name(self, username, user_name=username)
+        #     ecr_authenticators.add_user(user)
 
         self.container_repo.apply_removal_policy(RemovalPolicy.DESTROY)
