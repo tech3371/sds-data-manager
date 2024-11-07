e82bf97e1264ce543595d718f7263f7a56fabee5
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-11-07 16:01:56 -0700

    removing old spin DB table

--- sds_data_manager/lambda_code/SDSCode/database/models.py
+++ sds_data_manager/lambda_code/SDSCode/database/models.py
@@ -8,7 +8,6 @@ from enum import Enum
 
 import imap_data_access
 from sqlalchemy import (
-    Boolean,
     Column,
     DateTime,
     Identity,
@@ -66,23 +65,6 @@ class Base(DeclarativeBase):
     """Base class."""
 
     pass
-
-
-class UniversalSpinTable(Base):
-    """Universal Spin Table schema."""
-
-    __tablename__ = "universal_spin_table"
-    id = Column(Integer, primary_key=True)
-    spin_number = Column(Integer, nullable=False)
-    spin_start_sc_time = Column(Integer, nullable=False)
-    spin_start_utc_time = Column(DateTime, nullable=False)
-    star_tracker_flag = Column(Boolean, nullable=False)
-    spin_duration = Column(Integer, nullable=False)
-    thruster_firing_event = Column(Boolean, nullable=False)
-    repointing = Column(Boolean, nullable=False)
-    # TODO: create table for repointing and then make
-    # a foreign key to universal_spin_table
-    repointing_number = Column(Integer, nullable=False)
 
 
 class ProcessingJob(Base):
