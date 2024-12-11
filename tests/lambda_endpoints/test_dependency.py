9b2336e0a546766985951b07464d34f07928a183
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-12-11 13:35:37 -0700

    write tests for dependency

--- tests/lambda_endpoints/test_dependency.py
+++ tests/lambda_endpoints/test_dependency.py
@@ -1,17 +1,46 @@
 """Test data dependency functions."""
-# def test_get_downstream_dependencies(session):
-#     "Tests get_downstream_dependencies function."
-#     filename = "imap_hit_l1a_count-rates_20240101_v001.cdf"
-#     file_params = ScienceFilePath.extract_filename_components(filename)
 
-#     complete_dependents = get_downstream_dependencies(session, file_params)
-#     expected_complete_dependent = {
-#         "instrument": "hit",
-#         "data_level": "l1b",
-#         "descriptor": "all",
-#         "version": "v001",
-#         "start_date": "20240101",
-#     }
-#     assert len(complete_dependents) == 1
+import json
 
-#     assert complete_dependents[0] == expected_complete_dependent
+from sds_data_manager.lambda_code.SDSCode.pipeline_lambdas import dependency
+
+
+def test_get_downstream_dependencies():
+    "Tests get_downstream_dependencies function."
+    event = {
+        "dependency_type": "DOWNSTREAM",
+        "relationship": "HARD",
+        "data_source": "hit",
+        "data_type": "l1a",
+        "descriptor": "count-rates",
+    }
+
+    dependency_response = dependency.lambda_handler(event, None)
+    dependents = json.loads(dependency_response["body"])
+
+    expected_complete_dependent = [
+        {
+            "data_source": "hit",
+            "data_type": "l1b",
+            "descriptor": "all",
+        }
+    ]
+    assert len(dependents) == 1
+
+    assert dependents == expected_complete_dependent
+
+
+def test_lambda_handler_no_dependencies():
+    """Test lambda_handler when no dependencies are found."""
+    event = {
+        "data_source": "nonexistent",
+        "data_type": "l0",
+        "descriptor": "raw",
+        "dependency_type": "UPSTREAM",
+        "relationship": "HARD",
+    }
+
+    response = dependency.lambda_handler(event, None)
+
+    assert response["statusCode"] == 200
+    assert response["body"] == "[]"
