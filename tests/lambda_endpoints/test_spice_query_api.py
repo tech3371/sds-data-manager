ee98a55dac8812669393c498449c89ca4a5f1641
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2025-07-31 15:19:23 -0600

    wip

--- tests/lambda_endpoints/test_spice_query_api.py
+++ tests/lambda_endpoints/test_spice_query_api.py
@@ -182,7 +182,45 @@ def expected_ck_response():
                 "lsk_kernel": "imap_sclk_0012.tsc",
                 "ingestion_date": "2025-04-09, 21:12:53",
                 "timestamp": 1744233173.0,
-            }
+            },
+            {
+                "file_name": "lsk/naif0012.tls",
+                "file_root": "naif.tls",
+                "kernel_type": "leapseconds",
+                "version": 12,
+                "min_date_j2000": 0.0,
+                "max_date_j2000": 4575787269.183866,
+                "file_intervals_j2000": [[0, 4575787269.183866]],
+                "min_date_datetime": "2000-01-01, 12:00:00",
+                "max_date_datetime": "2145-01-01, 00:00:00",
+                "file_intervals_datetime": "[[2000-01-01T12:00:00, 2145-01-01T00:00:00]]",
+                "min_date_sclk": "1/0000000000:00000",
+                "max_date_sclk": "1/4285909749:39444",
+                "file_intervals_sclk": "[[1/0000000000:00000, 1/4285909749:39444]]",
+                "sclk_kernel": "/mnt/data/imap/spice/sclk/imap_sclk_0001.tsc",
+                "lsk_kernel": "/mnt/data/imap/spice/lsk/naif0012.tls",
+                "ingestion_date": "2025-04-30, 18:24:00",
+                "timestamp": 1746037440.0,
+            },
+            {
+                "file_name": "sclk/imap_sclk_0000.tsc",
+                "file_root": "imap_sclk_0000.tsc",
+                "kernel_type": "spacecraft_clock",
+                "version": 0,
+                "min_date_j2000": 315576066.1839245,
+                "max_date_j2000": 4575787269.183866,
+                "file_intervals_j2000": [[315576066.1839245, 4575787269.183866]],
+                "min_date_datetime": "2010-01-01, 00:00:00",
+                "max_date_datetime": "2145-01-01, 00:00:00",
+                "file_intervals_datetime": "[[2010-01-01T00:00:00, 2145-01-01T00:00:00]]",
+                "min_date_sclk": "1/0000000000:00000",
+                "max_date_sclk": "1/4285909749:39444",
+                "file_intervals_sclk": "[[1/0000000000:00000, 1/4285909749:39444]]",
+                "sclk_kernel": "/mnt/data/imap/spice/sclk/imap_sclk_0001.tsc",
+                "lsk_kernel": "/mnt/data/imap/spice/lsk/naif0012.tls",
+                "ingestion_date": "2025-04-30, 18:24:01",
+                "timestamp": 1746037441.0,
+            },
         ]
     )
     return expected_response
@@ -198,7 +236,7 @@ def test_query_result_body(session):
     assert json.loads(returned_query["body"])
 
 
-def test_file_name_query(session, expected_ck_response):
+def test_file_name_query(session):
     """Test that start date can be queried."""
     _insert_ck_test_data(session)
     event = {"queryStringParameters": {"file_name": "imap_2025_118_2025_120_009.ah.bc"}}
@@ -206,7 +244,44 @@ def test_file_name_query(session, expected_ck_response):
     returned_query = spice_query_api.lambda_handler(event=event, context={})
 
     assert returned_query["statusCode"] == 200
-    assert returned_query["body"] == expected_ck_response
+    assert returned_query["body"] == json.dumps(
+        [
+            {
+                "file_name": "ck/imap_2025_118_2025_120_009.ah.bc",
+                "file_root": "imap_2025_118_2025_120_.ah.bc",
+                "kernel_type": "attitude_history",
+                "version": 9,
+                "min_date_j2000": 799240876.0732585,
+                "max_date_j2000": 799244783.0732579,
+                "file_intervals_j2000": [
+                    [799240876.0732585, 799240921.0732585],
+                    [799242436.0732583, 799244783.0732579],
+                ],
+                "min_date_datetime": "2025-04-29, 23:20:06",
+                "max_date_datetime": "2025-04-30, 00:25:13",
+                "file_intervals_datetime": [
+                    [
+                        "2025-04-29T23:20:06.887765+00:00",
+                        "2025-04-29T23:20:51.887765+00:00",
+                    ],
+                    [
+                        "2025-04-29T23:46:06.887765+00:00",
+                        "2025-04-30T00:25:13.887765+00:00",
+                    ],
+                ],
+                "min_date_sclk": "1/0512204570:32482",
+                "max_date_sclk": "1/0512208477:32482",
+                "file_intervals_sclk": [
+                    ["1/0512204570:32482", "1/0512204615:32482"],
+                    ["1/0512206130:32482", "1/0512208477:32482"],
+                ],
+                "sclk_kernel": "naif0012.tls",
+                "lsk_kernel": "imap_sclk_0012.tsc",
+                "ingestion_date": "2025-04-09, 21:12:53",
+                "timestamp": 1744233173.0,
+            },
+        ]
+    )
 
 
 def test_start_time_query(session, expected_ck_response):
@@ -215,7 +290,6 @@ def test_start_time_query(session, expected_ck_response):
     event = {"queryStringParameters": {"start_time": "0"}}
 
     returned_query = spice_query_api.lambda_handler(event=event, context={})
-    print(returned_query["body"])
     assert returned_query["statusCode"] == 200
     assert returned_query["body"] == expected_ck_response
 
@@ -237,7 +311,6 @@ def test_start_and_end_time_query(session, expected_ck_response):
     event = {"queryStringParameters": {"start_time": "0", "end_time": "1000000000"}}
     _insert_ck_test_data(session)
     returned_query = spice_query_api.lambda_handler(event=event, context={})
-    print(returned_query["body"])
     assert returned_query["statusCode"] == 200
     assert returned_query["body"] == expected_ck_response
 
@@ -246,7 +319,49 @@ def test_empty_start_time_query(session):
     """Test that a start_date query with no matches returns an empty list."""
     _insert_ck_test_data(session)
     event = {"queryStringParameters": {"start_time": "1000000000"}}
-    expected_response = json.dumps([])
+    expected_response = json.dumps(
+        [
+            # Leap seconds and spacecraft clock
+            {
+                "file_name": "lsk/naif0012.tls",
+                "file_root": "naif.tls",
+                "kernel_type": "leapseconds",
+                "version": 12,
+                "min_date_j2000": 0.0,
+                "max_date_j2000": 4575787269.183866,
+                "file_intervals_j2000": [[0, 4575787269.183866]],
+                "min_date_datetime": "2000-01-01, 12:00:00",
+                "max_date_datetime": "2145-01-01, 00:00:00",
+                "file_intervals_datetime": "[[2000-01-01T12:00:00, 2145-01-01T00:00:00]]",
+                "min_date_sclk": "1/0000000000:00000",
+                "max_date_sclk": "1/4285909749:39444",
+                "file_intervals_sclk": "[[1/0000000000:00000, 1/4285909749:39444]]",
+                "sclk_kernel": "/mnt/data/imap/spice/sclk/imap_sclk_0001.tsc",
+                "lsk_kernel": "/mnt/data/imap/spice/lsk/naif0012.tls",
+                "ingestion_date": "2025-04-30, 18:24:00",
+                "timestamp": 1746037440.0,
+            },
+            {
+                "file_name": "sclk/imap_sclk_0000.tsc",
+                "file_root": "imap_sclk_0000.tsc",
+                "kernel_type": "spacecraft_clock",
+                "version": 0,
+                "min_date_j2000": 315576066.1839245,
+                "max_date_j2000": 4575787269.183866,
+                "file_intervals_j2000": [[315576066.1839245, 4575787269.183866]],
+                "min_date_datetime": "2010-01-01, 00:00:00",
+                "max_date_datetime": "2145-01-01, 00:00:00",
+                "file_intervals_datetime": "[[2010-01-01T00:00:00, 2145-01-01T00:00:00]]",
+                "min_date_sclk": "1/0000000000:00000",
+                "max_date_sclk": "1/4285909749:39444",
+                "file_intervals_sclk": "[[1/0000000000:00000, 1/4285909749:39444]]",
+                "sclk_kernel": "/mnt/data/imap/spice/sclk/imap_sclk_0001.tsc",
+                "lsk_kernel": "/mnt/data/imap/spice/lsk/naif0012.tls",
+                "ingestion_date": "2025-04-30, 18:24:01",
+                "timestamp": 1746037441.0,
+            },
+        ]
+    )
     returned_query = spice_query_api.lambda_handler(event=event, context={})
 
     assert returned_query["statusCode"] == 200
@@ -257,7 +372,48 @@ def test_empty_end_date_query(session):
     """Test that an end_time query with no matches returns an empty list."""
     _insert_ck_test_data(session)
     event = {"queryStringParameters": {"end_time": "0"}}
-    expected_response = json.dumps([])
+    expected_response = json.dumps(
+        [
+            {
+                "file_name": "lsk/naif0012.tls",
+                "file_root": "naif.tls",
+                "kernel_type": "leapseconds",
+                "version": 12,
+                "min_date_j2000": 0.0,
+                "max_date_j2000": 4575787269.183866,
+                "file_intervals_j2000": [[0, 4575787269.183866]],
+                "min_date_datetime": "2000-01-01, 12:00:00",
+                "max_date_datetime": "2145-01-01, 00:00:00",
+                "file_intervals_datetime": "[[2000-01-01T12:00:00, 2145-01-01T00:00:00]]",
+                "min_date_sclk": "1/0000000000:00000",
+                "max_date_sclk": "1/4285909749:39444",
+                "file_intervals_sclk": "[[1/0000000000:00000, 1/4285909749:39444]]",
+                "sclk_kernel": "/mnt/data/imap/spice/sclk/imap_sclk_0001.tsc",
+                "lsk_kernel": "/mnt/data/imap/spice/lsk/naif0012.tls",
+                "ingestion_date": "2025-04-30, 18:24:00",
+                "timestamp": 1746037440.0,
+            },
+            {
+                "file_name": "sclk/imap_sclk_0000.tsc",
+                "file_root": "imap_sclk_0000.tsc",
+                "kernel_type": "spacecraft_clock",
+                "version": 0,
+                "min_date_j2000": 315576066.1839245,
+                "max_date_j2000": 4575787269.183866,
+                "file_intervals_j2000": [[315576066.1839245, 4575787269.183866]],
+                "min_date_datetime": "2010-01-01, 00:00:00",
+                "max_date_datetime": "2145-01-01, 00:00:00",
+                "file_intervals_datetime": "[[2010-01-01T00:00:00, 2145-01-01T00:00:00]]",
+                "min_date_sclk": "1/0000000000:00000",
+                "max_date_sclk": "1/4285909749:39444",
+                "file_intervals_sclk": "[[1/0000000000:00000, 1/4285909749:39444]]",
+                "sclk_kernel": "/mnt/data/imap/spice/sclk/imap_sclk_0001.tsc",
+                "lsk_kernel": "/mnt/data/imap/spice/lsk/naif0012.tls",
+                "ingestion_date": "2025-04-30, 18:24:01",
+                "timestamp": 1746037441.0,
+            },
+        ]
+    )
     returned_query = spice_query_api.lambda_handler(event=event, context={})
 
     assert returned_query["statusCode"] == 200
